9af1312391f5ba04177c760613c8bf658d7c9b14
thodoris-mar <cs05426@uoi.gr>
2024-10-13 20:23:53 +0300

    Update lab01.s

--- lab01.s
+++ lab01.s
@@ -11,7 +11,7 @@
 matric: # Words ending with ':' are **labels**
         # The common convention is to write labels starting from the leftmost column.
         # Try to keep label names short
-        .word 30  # This directive reserves enough space for a word in memory
+        .word 5426  # This directive reserves enough space for a word in memory
                   #  and initializes it with the specific value.
 matricplus1:
         .word 0   
@@ -55,7 +55,8 @@ prog:   # Labels in code are used for control flow: if/then/else, loops, etc.
         #  test.
         # Labels do not take up space in memory. They are only used by the assembler
 
-        li         s0, 100         # Get value 100 into register s0. li - load immediate
+        la         a0, matric        # Get value 100 into register s0. li - load immediate
+        lw         s0, 0(a0)         # get the value of matric in s0
         
         lw         s1, 0(a1)       # s1 gets the value of var1. a1 has the address of var1
         add        s1, s1,   s0    # s1 = var1 + s0
